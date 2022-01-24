b57d18f1001c3f83e773a3b14ebfd703940097d4
stesuu <stenver.suurkutt@kambi.com>
2022-01-24 17:04:53 +0200

    fixed autofocus and submit with enter issues, added done button

--- src/index.js
+++ src/index.js
@@ -1,28 +1,55 @@
 const container = document.getElementById('container')
+var count = -1
 
 const newTodo = () => {
+    count += 1
     let newTodoDiv = document.createElement('div')
-    let todo = document.createElement('textarea')
+    let input = document.createElement('textarea')
     let enter = document.createElement('input')
     let cancel = document.createElement('input')
     newTodoDiv.className = "todo"
     enter.className = "enter"
-    todo.className = "input"
+    input.className = "input"
     cancel.className = "cancel"
-    todo.type = "text"
+    input.type = "text"
     enter.type = "submit"
     enter.value = "Add todo event"
     cancel.type = "button"
     cancel.value = "Cancel"
-    newTodoDiv.appendChild(todo)
+    newTodoDiv.appendChild(input)
     newTodoDiv.appendChild(enter)
     newTodoDiv.appendChild(cancel)
     container.appendChild(newTodoDiv)
+
+    input.focus()
+    input.addEventListener("keypress", (e) => {
+        if (e.key == "Enter") {
+            newTodoDiv.style.display = "none"
+            let newCardDiv = document.createElement('div')
+            let card = document.createElement('textarea')
+            let done = document.createElement('button')
+            newCardDiv.className = "carddiv"
+            done.className = "done"
+            done.type = "button"
+            done.textContent = "x"
+            card.id = count
+            card.className = "card"
+            card.textContent = input.value
+            card.spellcheck = false
+            newCardDiv.appendChild(card)
+            newCardDiv.appendChild(done)
+            container.appendChild(newCardDiv)
+            todoBtn.style.display = "block"
+        }
+    })
+
     enter.onclick = () => {
         newTodoDiv.style.display = "none"
-        let card = document.createElement('p')
+        let card = document.createElement('textarea')
+        card.id = count
         card.className = "card"
         card.textContent = todo.value
+        card.spellcheck = false
         container.appendChild(card)
         todoBtn.style.display = "block"
 
@@ -36,14 +63,27 @@ const newTodo = () => {
 
 }
 
-const todoBtn = document.getElementById('todo')
+const todoBtn = document.getElementById('newtodobtn')
 
 todoBtn.addEventListener("click", (e) => {
     todoBtn.style.display = "none"
     newTodo()
 })
 
-console.log("to do")
+
+container.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
+        var cards = document.getElementsByClassName('card')
+        for (var i = 0; i < cards.length; i++) {
+            if (cards[i].id == i) {
+                cards[i].blur()
+            }
+        }
+    }
+})
+
+
+//form bottom right
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
